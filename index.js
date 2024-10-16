eba4d518f0b5a74603cfa6e302deeb8d81a43c5c
Prateek Shrivastava <iamprateek.1108@gmail.com>
2024-10-16 15:39:37 +0530

    Github data added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const githubData = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 95,
+    "public_gists": 5,
+    "followers": 32352,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-10-13T08:22:22Z"
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +48,10 @@ app.get('/about',(req,res) => {
 
 app.get("/contact",(req,res) => {
     res.send("Contact Page !")
+})
+
+app.get("/github",(req,res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
